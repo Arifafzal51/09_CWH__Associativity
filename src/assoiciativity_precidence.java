5b476521563933366ec9b055b4ea52bd54da3ce5
arif afzal <arif.afzal_cs20@gla.ac.in>
2021-11-28 16:37:57 +0530

    Initial commit

---
+++ src/assoiciativity_precidence.java
@@ -0,0 +1,19 @@
+public class assoiciativity_precidence {
+    public static void main(String[] args) {
+        int a= 60*5-(34/2);
+        int b= 60/5-34*2;
+        int c =4 ;
+
+      //  System.out.println(a+b);
+       //  System.out.println(b);
+        int x =6;
+        int y =1;
+        //int k= x*y/2;
+        int k = b * b - (4 * a * c) / (2 * a);
+        System.out.println(k);
+
+
+
+
+    }
+}
---
+++ src/increment_decrement.java
@@ -0,0 +1,10 @@
+public class increment_decrement {
+    public static void main(String[] args) {
+      int y = 7;
+      int x = y++*8;
+        System.out.println(x);
+        System.out.println(++x);
+
+
+    }
+}
